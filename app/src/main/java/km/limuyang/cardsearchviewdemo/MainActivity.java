922dbf82645dfa9cf578b79fafc42851bfeae73a
limuyang <limuyang2@live.com>
2017-07-30 23:19:23 +0800

    初步创建demo

--- app/src/main/java/km/limuyang/cardsearchviewdemo/MainActivity.java
+++ app/src/main/java/km/limuyang/cardsearchviewdemo/MainActivity.java
@@ -1,13 +1,46 @@
 package km.limuyang.cardsearchviewdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import km.lmy.searchview.SearchView;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.activity_toolbar)
+    Toolbar activityToolbar;
+    @BindView(R.id.searchView)
+    SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        activityToolbar.setTitle(this.getTitle());
+        setSupportActionBar(activityToolbar);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);//加载menu文件到布局
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                searchView.autoOpenOrClose();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
