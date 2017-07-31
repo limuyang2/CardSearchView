289a5ddb9f7722488d0121f4adde02bdb73b71ed
limuyang <limuyang2@live.com>
2017-07-31 14:15:22 +0800

    完善demo

--- app/src/main/java/km/limuyang/cardsearchviewdemo/MainActivity.java
+++ app/src/main/java/km/limuyang/cardsearchviewdemo/MainActivity.java
@@ -1,10 +1,16 @@
 package km.limuyang.cardsearchviewdemo;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -24,7 +30,57 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         activityToolbar.setTitle(this.getTitle());
+        activityToolbar.setTitleTextColor(Color.WHITE);
         setSupportActionBar(activityToolbar);
+
+
+        List<String> historyList = new ArrayList<>();
+        historyList.add("Joker");
+        historyList.add("Harry");
+        historyList.add("Kate");
+        historyList.add("Alice");
+        //设置全新的历史记录列表
+        searchView.setNewHistoryList(historyList);
+
+        //添加一条历史纪录
+        searchView.addOneHistory("Jenson");
+
+
+        //设置历史纪录点击事件
+        searchView.setHistoryItemClickListener(new SearchView.OnHistoryItemClickListener() {
+            @Override
+            public void onClick(String historyStr, int position) {
+                Toast.makeText(MainActivity.this, historyStr, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //设置软键盘搜索按钮点击事件
+        searchView.setOnSearchActionListener(new SearchView.OnSearchActionListener() {
+            @Override
+            public void onSearchAction(String searchText) {
+                Toast.makeText(MainActivity.this, "搜索-->" + searchText, Toast.LENGTH_SHORT).show();
+                searchView.addOneHistory(searchText);
+            }
+        });
+
+
+        //设置输入文本监听事件
+        searchView.setOnInputTextChangeListener(new SearchView.OnInputTextChangeListener() {
+            @Override
+            public void onTextChanged(CharSequence charSequence) {
+                //TODO something
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence charSequence) {
+                //TODO something
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                //TODO something
+            }
+        });
     }
 
 
@@ -38,9 +94,12 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_search:
+                //自动打开关闭SearchView
                 searchView.autoOpenOrClose();
                 break;
         }
         return super.onOptionsItemSelected(item);
     }
+
+
 }
